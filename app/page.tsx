255380a85dc4dbe635037374bc26ff7aa96fe336
Alejandro <asaavedram@cuc.edu.co>
2026-09-10 01:32:24 -0500

    feat: made delete funtional buttom

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  function deleteTask(id: string) {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    }
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -60,9 +63,17 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+  
             Documentation
           </a>
         </div>
+      </main>
+    </div>
+  );
+              </button>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
